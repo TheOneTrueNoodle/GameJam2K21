09867b9eb2209c9a679e0eafa73f601a654b8576
MonkeyZebraProductions <73033054+MonkeyZebraProductions@users.noreply.github.com>
2021-01-30 10:58:22 +0000

    Physics Material Added

--- Returned/Assets/Scripts/StealieScript2.cs
+++ Returned/Assets/Scripts/StealieScript2.cs
@@ -6,6 +6,9 @@ public class StealieScript2 : MonoBehaviour
 {
     public Transform Player;
     public float Speed;
+
+    private bool _isFollow;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +18,25 @@ public class StealieScript2 : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, Player.position,Speed*Time.deltaTime);
+        if(_isFollow)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.position, Speed * Time.deltaTime);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if(collision.gameObject.tag=="Item")
+        {
+
+        }
+    }
+
+    IEnumerator Stunned()
+    {
+        _isFollow = false;
+        yield return new WaitForSeconds(3.0f);
+        _isFollow = true;
+
     }
 }
